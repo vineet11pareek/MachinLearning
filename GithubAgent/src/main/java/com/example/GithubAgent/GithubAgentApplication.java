2cc7d600976152075d1eaa1c32147f169c57f93f
vineet11pareek <vineet00pareek@gmail.com>
2025-12-22 11:03:12 +0100

    setup a base project for github agent using MCP client

---
+++ GithubAgent/src/main/java/com/example/GithubAgent/GithubAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.GithubAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAgentApplication.class, args);
+	}
+
+}
---
+++ GithubAgent/src/test/java/com/example/GithubAgent/GithubAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GithubAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
